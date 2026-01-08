757d1dcbc64fdaf33afcd5ccbd73eeb46671f9b3
Zankyu1w0 <supertinom1@gmail.com>
2026-01-08 04:15:07 +0300

    Update MainActivity.kt

--- app/src/main/java/com/exoiptv/MainActivity.kt
+++ app/src/main/java/com/exoiptv/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.exoiptv
 
-import android.net.Uri
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.ui.PlayerView
@@ -10,23 +9,21 @@ import com.google.android.exoplayer2.ui.PlayerView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var player: ExoPlayer
-    private lateinit var playerView: PlayerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        playerView = PlayerView(this)
+        val playerView = PlayerView(this)
         setContentView(playerView)
 
         player = ExoPlayer.Builder(this).build()
         playerView.player = player
 
-        val url = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+        val url = "https://tv-trt1.medya.trt.com.tr/master.m3u8"
 
-        val mediaItem = MediaItem.fromUri(Uri.parse(url))
+        val mediaItem = MediaItem.fromUri(url)
         player.setMediaItem(mediaItem)
         player.prepare()
-        player.play()
+        player.playWhenReady = true
     }
 
     override fun onStop() {
